135c5b5e7f14cce25894561e9a66e799b4842f71
Samantha Ramos <samantharamos@samanthas-air.attlocal.net>
2017-09-13 12:00:17 -0500

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'elderhelperappapi.herokuapp.com', 'localhost:3004'  
+    origins 'elderhelper.herokuapp.com', 'localhost:3004'  
 
     resource '*',
       headers: :any,
